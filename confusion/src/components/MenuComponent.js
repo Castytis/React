b4b5a28a8629eb32f3302dea6e8fa6ef090ff2f9
kasty <kastytis.slubelis@gmail.com>
2020-07-02 23:07:10 +0300

    fixing MenuComponent

--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -3,31 +3,6 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 're
 
 
 class Menu extends Component {
-    constructor(props) {
-        super(props);
-       
-    }
- 
-   
-
-    renderDish(dish) {
-        if (dish != null) {
-            return(
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />  
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>      
-                </Card>
-            )
-        }
-        else{
-            return(
-                <div></div>
-            );
-        }
-    }
 
     render() {
         const menu = this.props.dishes.map((dish) => {
