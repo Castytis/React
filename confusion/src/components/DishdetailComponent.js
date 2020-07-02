2fc6a6628320a37f50f87c71b6abfd5ee5041073
kasty <kastytis.slubelis@gmail.com>
2020-07-02 23:27:57 +0300

    MenuComponent functional componen

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class Dishdetail extends Component {
 
-    renderComments(comments) {
+    function RenderComments({comments}) {
 
         if (comments == null) {
             return (<div></div>)
@@ -35,7 +34,7 @@ class Dishdetail extends Component {
         )
     }
 
-    renderDish(dish) {
+    function RenderDish({dish}) {
         if (dish != null) {
             return (
                 <div className='col-12 col-md-5 m-1'>
@@ -56,21 +55,21 @@ class Dishdetail extends Component {
         }
     }
 
-    render() {
-        const dish = this.props.dish
-        if (dish == null) {
+    const Dishdetail = (props) => {
+        if (props.dish == null) {
             return (<div></div>)
         }
 
-        const dishItem = this.renderDish(dish)
-        const commentItem = this.renderComments(dish.comments)
+       
         return (
             <div className='row'>
-                {dishItem}
-                {commentItem}
-            </div>
+                <RenderDish dish={props.dish} />
+                <RenderComments comments={props.dish.comments} />
+                </div>
         )
     }
-}
+
+
+
 
 export default Dishdetail 
