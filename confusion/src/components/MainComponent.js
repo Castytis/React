d676d88be3b4da448c29718de31971723f9ef2fe
kasty <kastytis.slubelis@gmail.com>
2020-07-12 17:17:41 +0300

    dishes

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -6,6 +6,7 @@ import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
 import {Switch, Route, Redirect} from 'react-router-dom';
+import Contact from './ContactComponent';
 
 class Main extends Component {
 
@@ -29,6 +30,7 @@ class Main extends Component {
           <Switch>
             <Route path="/home" component = {HomePage} />
             <Route exact path="/menu" component = {() => <Menu dishes={this.state.dishes}/>} />  
+            <Route exact path="/contactus" component ={Contact} />
             <Redirect to="/home" />
           </Switch>
         <Footer />
