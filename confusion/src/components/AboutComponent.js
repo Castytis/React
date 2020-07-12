913f5f5e2793619a14a62b24c22d10a28bfda5b1
kasty <kastytis.slubelis@gmail.com>
2020-07-12 22:57:34 +0300

    Task 2

--- confusion/src/components/AboutComponent.js
+++ confusion/src/components/AboutComponent.js
@@ -1,14 +1,35 @@
 import React from 'react';
-import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
+import { Breadcrumb, BreadcrumbItem, CardTitle,  Card, CardBody,CardImg, CardImgOverlay, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+
 
 function About(props) {
 
     const leaders = props.leaders.map((leader) => {
         return (
-            <p>Leader {leader.name}</p>
+            <div key={leader.id} className="mt-5"> 
+                      <RenderLeader leader ={leader} />
+                  </div>
         );
     });
+
+    function RenderLeader({leader}) {
+        return (
+            <div key={leader.id} className="col-12 mt-5">
+              <Media tag="li">
+                <Media left middle>
+                    <Media object src={leader.image} alt={leader.name} />
+                </Media>
+                <Media body className="ml-5">
+                  <Media heading>{leader.name}</Media>
+                  <p>{leader.designation}</p>
+                  <p>{leader.description}</p>
+                </Media>
+              </Media>
+            </div>
+          );
+    }
+
 
     return(
         <div className="container">
--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -7,9 +7,10 @@ import Home from "./HomeComponent";
 import {Switch, Route, Redirect} from 'react-router-dom';
 import Contact from './ContactComponent';
 import { DISHES } from '../shared/dishes';
-import { COMMECTS, COMMENTS } from '../shared/comments';
+import { COMMENTS } from '../shared/comments';
 import { LEADERS } from '../shared/leaders';
 import { PROMOTIONS } from '../shared/promotions';
+import About from "./AboutComponent";
 
 class Main extends Component {
 
@@ -47,6 +48,7 @@ class Main extends Component {
         <Header/>
           <Switch>
             <Route path="/home" component = {HomePage} />
+            <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders}/>}/>
             <Route exact path="/menu" component = {() => <Menu dishes={this.state.dishes}/>} />  
             <Route exact path="/contactus" component ={Contact} />
             <Route path="/menu/:dishId" component ={DishWithId} />
