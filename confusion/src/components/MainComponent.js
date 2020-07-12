e01377aeb7dfff8e696830fbc0af01f564d0bda1
kasty <kastytis.slubelis@gmail.com>
2020-07-12 17:46:02 +0300

    Route

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import { DISHES } from '../shared/dishes';
 import Menu from './MenuComponent'
 import Dishdetail from './DishdetailComponent';
 import Header from "./HeaderComponent";
@@ -7,6 +6,10 @@ import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
 import {Switch, Route, Redirect} from 'react-router-dom';
 import Contact from './ContactComponent';
+import { DISHES } from '../shared/dishes';
+import { COMMECTS, COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 
 class Main extends Component {
 
@@ -15,13 +18,19 @@ class Main extends Component {
 
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     }
   }
 
   render() {
     const HomePage = () => {
       return(
-        <Home />
+        <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]} 
+        promotion={this.state.promotions.filter((promo) => promo.featured)[0]} 
+        leader={this.state.leaders.filter((leader) => leader.featured)[0]} 
+        />
       );
     }
     return (
