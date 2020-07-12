270b32e39e1219b9a0eedddb337e71078edbe45f
kasty <kastytis.slubelis@gmail.com>
2020-07-12 16:50:13 +0300

    NavBar

--- confusion/src/components/HeaderComponent.js
+++ confusion/src/components/HeaderComponent.js
@@ -3,15 +3,32 @@ import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, NavLink, Ju
 import {Navlink} from 'react-router-dom'
 
 class Header extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            isNavOpen: false
+        }
+        this.toggleNav = this.toggleNav.bind(this);
+    }
+
+    toggleNav() {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        })
+    }
+
     render() {
         return(
             <React.Fragment> 
                 <Navbar dark expand="md">
                     <div className="container">
+                        <NavbarToggler onClick={this.toggleNav} />
                         <NavbarBrand className="mr-auto" href="/">
                             <img src = "assets/images/logo.png" height ="30" width="41"
                             alt ="Ristorante Con Fusion" />
                         </NavbarBrand>
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
                             <Nav navbar> 
                                 <NavItem>
                                     <NavLink className="nav-link" to = "/home">
@@ -34,6 +51,7 @@ class Header extends Component {
                                     </NavLink>
                                 </NavItem>
                             </Nav>
+                            </Collapse>
                     </div>
                 </Navbar>
                 <Jumbotron>
