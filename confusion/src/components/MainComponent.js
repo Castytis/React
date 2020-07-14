ce32b6f57dee13eb8f97df4bb26a2b86f73f8999
kasty <kastytis.slubelis@gmail.com>
2020-07-14 21:57:29 +0300

    configure\store

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -6,24 +6,15 @@ import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
 import {Switch, Route, Redirect} from 'react-router-dom';
 import Contact from './ContactComponent';
-import { DISHES } from '../shared/dishes';
-import { COMMENTS } from '../shared/comments';
-import { LEADERS } from '../shared/leaders';
-import { PROMOTIONS } from '../shared/promotions';
 import About from "./AboutComponent";
+
 
 class Main extends Component {
 
   constructor(props) {
     super(props);
-
-    this.state = {
-      dishes: DISHES,
-      comments: COMMENTS,
-      promotions: PROMOTIONS,
-      leaders: LEADERS
-    }
   }
+  
 
   render() {
     const HomePage = () => {
